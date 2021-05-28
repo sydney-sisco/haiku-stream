9364e3399eb3cd98acaeba51f024ae5221246e80
Sydney Sisco <hello@sydsis.co>
2021-05-28 11:29:34 -0700

    add tests for countSyllables helper function

--- src/helpers/syllables.test.js
+++ src/helpers/syllables.test.js
@@ -1,9 +1,26 @@
-import { arrangeSyllables } from "../helpers/syllables";
+import { arrangeSyllables, countSyllables } from "../helpers/syllables";
 
-const words = []
+test('countSyllables returns null if passed nothing', () => {
+  const result = countSyllables();
+  expect(result).toBeNull();
+});
 
+test('countSyllables counts spaces correctly', () => {
+  const result = countSyllables('this or that');
+  expect(result).toBe(3);
+});
 
-test("arrangeSyllables returns an array", () => {
-  const result = arrangeSyllables();
-  expect(Array.isArray(result)).toBe(true);
+test('countSyllables counts syllables of 1 word', () => {
+  const result = countSyllables('mountainous');
+  expect(result).toBe(3);
+});
+
+test('countSyllables counts syllables of multiple words', () => {
+  const result = countSyllables('mountainous region');
+  expect(result).toBe(5);
+});
+
+test('countSyllables trims whitespace', () => {
+  const result = countSyllables(' mountainous region ');
+  expect(result).toBe(5);
 });
