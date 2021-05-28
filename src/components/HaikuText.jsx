2c9ed2f70b3dbc35b55d941772540ee3dbb9e992
Sydney Sisco <hello@sydsis.co>
2021-05-27 18:16:03 -0700

    split syllables using hyphen package

--- src/components/HaikuText.jsx
+++ src/components/HaikuText.jsx
@@ -1,18 +1,38 @@
+import { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import styles from './HaikuText.module.css';
+import { hyphenate } from "hyphen/en";
 
 
 export default function HaikuText (){
+  const [text, setText] = useState('');
+  const [hyphenatedText, setHyphenatedText] = useState('');
 
+  useEffect(() => {
+    hyphenate(text, { hyphenChar: "%" })
+    .then(result => {
+      setHyphenatedText(() => result);
+      console.log(result);
+    })
+  }, [text]);
+
+  const handleChange = event => {
+    setText(() => event.target.value);
+  }
 
   return (
-    <TextField
-      id="outlined-multiline-static"
-      label="Multiline"
-      multiline
-      rows={3}
-      defaultValue="Default Value"
-      variant="outlined"
-    />
+    <>
+      <TextField
+        id="outlined-multiline-static"
+        variant="outlined"
+        // label="Multiline"
+        // defaultValue="Default Value"
+        multiline
+        rows={3}
+        value={text}
+        onChange={handleChange}
+      />
+      <p>{hyphenatedText}</p>
+    </>
   )
 }
